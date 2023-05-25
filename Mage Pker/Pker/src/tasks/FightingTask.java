19234425e7ea6d3ae8952145170f7af8d1a431e2
DynoDynoDynoDyno <134567114+DynoDynoDynoDyno@users.noreply.github.com>
2023-05-25 01:28:52 -0400

    Add files via upload
    New updates, trying to detect potential targets and attack them.

--- Mage Pker/Pker/src/tasks/FightingTask.java
+++ Mage Pker/Pker/src/tasks/FightingTask.java
@@ -2,6 +2,8 @@ package tasks;
 
 import org.dreambot.api.methods.combat.Combat;
 import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.wrappers.interactive.Player;
+import org.dreambot.api.methods.interactive.Players;
 
 import static org.dreambot.api.utilities.Logger.log;
 
@@ -14,12 +16,25 @@ public class FightingTask {
     }
 
     public void execute() {
+        Player localPlayer = Players.getLocal();
+        int localPlayerCombatLevel = localPlayer.getLevel();
+        int wildernessLevel = Combat.getWildernessLevel();
+
         int currentHPPercent = Combat.getHealthPercent();
         log("Current HP: " + currentHPPercent + "%");
 
         if (currentHPPercent <= 50) {
             log("HP is 50% or less. Eating " + foodName);
             Inventory.interact(foodName);  // Replace 'foodName' with the name of your food
+        }
+
+        Player targetPlayer = Players.closest(p -> p != null && !p.equals(localPlayer) &&
+                Math.abs(p.getLevel() - localPlayerCombatLevel) <= wildernessLevel &&
+                !p.isInCombat());
+
+        if (targetPlayer != null) {
+            log("Found player to attack: " + targetPlayer.getName());
+            targetPlayer.interact("Attack");
         }
     }
 }
